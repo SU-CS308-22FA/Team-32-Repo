b906ec003e03b604ab6a43476304f8cd77b26022
Hyphecito <berkekeskinler@sabanciuniv.edu>
2022-11-03 21:32:34 +0300

    TeamRegistry/Login + HomePage

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
-router.get('/', function (req, res, next) {
+router.get('/register', function (req, res, next) {
 	return res.render('index.ejs');
 });
 
 
-router.post('/', function(req, res, next) {
+router.post('/register', function(req, res, next) {
 	console.log(req.body);
 	var personInfo = req.body;
 
@@ -77,7 +77,7 @@ router.post('/login', function (req, res, next) {
 				res.send({"Success":"Wrong password!"});
 			}
 		}else{
-			res.send({"Success":"This Email Is not regestered!"});
+			res.send({"Success":"This Email Is not registered!"});
 		}
 	});
 });
@@ -92,6 +92,20 @@ router.get('/profile', function (req, res, next) {
 		}else{
 			//console.log("found");
 			return res.render('data.ejs', {"name":data.username,"email":data.email, "teamname":data.teamname});
+		}
+	});
+});
+
+router.get('/teamprofile', function (req, res, next) {
+	console.log("teamprofile");
+	User.findOne({unique_id:req.session.userId},function(err,data){
+		console.log("data");
+		console.log(data);
+		if(!data){
+			res.redirect('/');
+		}else{
+			//console.log("found");
+			return res.render('teamdata.ejs', {"name":data.username,"email":data.email});
 		}
 	});
 });
@@ -140,6 +154,91 @@ router.post('/forgetpass', function (req, res, next) {
 		}
 	});
 	
+});
+
+router.get('/teamlogin', function (req, res, next) {
+	return res.render('teamlogin.ejs');
+});
+
+router.post('/teamlogin', function (req, res, next) {
+	//console.log(req.body);
+	User.findOne({email:req.body.email},function(err,data){
+		if(data){
+			
+			if(data.password==req.body.password){
+				//console.log("Done Login");
+				req.session.userId = data.unique_id;
+				//console.log(req.session.userId);
+				res.send({"Success":"Success!"});
+				
+			}else{
+				res.send({"Success":"Wrong password!"});
+			}
+		}else{
+			res.send({"Success":"This Email Is not registered!"});
+		}
+	});
+});
+
+
+router.get('/', function (req, res, next) {
+	return res.render('home.ejs');
+});
+
+router.get('/teamregister', function (req, res, next) {
+	return res.render('teamregister.ejs');
+});
+
+
+router.post('/teamregister', function(req, res, next) {
+	console.log(req.body);
+	var personInfo = req.body;
+
+
+	if(!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf || !personInfo.teamname){
+		res.send();
+	} else {
+		if (personInfo.password == personInfo.passwordConf) {
+
+			User.findOne({email:personInfo.email},function(err,data){
+				if(!data){
+					var c;
+					User.findOne({},function(err,data){
+
+						if (data) {
+							console.log("if");
+							c = data.unique_id + 1;
+						}else{
+							c=1;
+						}
+
+						var newPerson = new User({
+							unique_id:c,
+							email:personInfo.email,
+							username: personInfo.username,
+							password: personInfo.password,
+							passwordConf: personInfo.passwordConf,
+							teamname: personInfo.teamname
+						});
+
+						newPerson.save(function(err, Person){
+							if(err)
+								console.log(err);
+							else
+								console.log('Success');
+						});
+
+					}).sort({_id: -1}).limit(1);
+					res.send({"Success":"You are registered,You can login now."});
+				}else{
+					res.send({"Success":"Email is already used."});
+				}
+
+			});
+		}else{
+			res.send({"Success":"password is not matched"});
+		}
+	}
 });
 
 module.exports = router;
