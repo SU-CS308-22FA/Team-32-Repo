1fcbdcd46018ca92d467fff2a3035089523aec90
Yigit Gumuslu <gumusluyigit@sabanciuniv.edu>
2022-12-13 17:31:20 +0300

    proje

--- backend/base/urls/product_urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from base.views import product_views as views
-
-urlpatterns = [
-    path('test/', views.getRoutes, name="routes"),
-    path('', views.getProducts, name="products"),
-    path('<str:pk>', views.getProduct, name="product"),
-]
