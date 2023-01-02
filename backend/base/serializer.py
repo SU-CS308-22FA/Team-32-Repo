3f513a374bdbfee4f2d21000ad14bd72938f09b1
Yigit Gumuslu <gumusluyigit@sabanciuniv.edu>
2023-01-02 18:11:22 +0300

    orderpage backend

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Product
+from .models import Product, ShippingAddress, Order, OrderItem
 from dataclasses import field, fields
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -44,3 +44,41 @@ class UserSerializerWithToken(UserSerializer):
     def get_token(self, obj):
         token = RefreshToken.for_user(obj)
         return str(token.access_token)
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress 
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+        
+class OrderSerializer(serializers.ModelSerializer):
+    orderItems = serializers.SerializerMethodField(read_only = True)
+    shippingAddress = serializers.SerializerMethodField(read_only = True)
+    user = serializers.SerializerMethodField(read_only = True)
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def get_orderItems(self, obj):
+        items = obj.orderitem_set.all()
+        serializers = OrderItemSerializer(items, many = True)
+
+        return serializers.data
+
+    def get_shippingAddress(self, obj):
+        try:
+            address = ShippingAddressSerializer(obj.shippingAddress, many=False).data
+
+        except:
+            address = False
+
+        return address
+
+    def get_user(self, obj):
+        user = obj.user
+        serializers = UserSerializer(user, many = False)
+        return serializers
---
+++ backend/base/views/order_views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base.models import Order, ShippingAddress, Product, OrderItem
+from base.serializer import OrderSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import permission_classes
+from rest_framework import status
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'Out of stock.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    else:
+        # create order
+        order = Order.objects.create(
+            user=user,
+            paymentMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice=data['shippingPrice'],
+            TotalPrice=data['TotalPrice'],
+        )
+    # create address
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country'],
+        )
+
+    # order orderitem set
+        for i in orderItems:
+            product = Product.objects.get(_id=i['product'])
+
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url
+            )
+
+    # update stock
+            product.countInStock -= item.qty
+            product.save()
+
+        serializer = OrderSerializer(order, many=False)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
